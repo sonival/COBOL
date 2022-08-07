e89e8f8efdc06b3ac08e911d8a3820484628744d
soni <soni@live.com>
2022-08-06 23:19:33 -0300

    feat: calcular diferenca percentual

--- calc1.cbl
+++ calc1.cbl
@@ -1,15 +1,28 @@
         identification division.
         program-id. CALC1.
-        *---  ESTE PROGRAMA CALCULAR DIFERENCA PERCENTUAL ENTRE VALOR 
         
         environment division.
         configuration section.
         
         data division.
         working-storage section.
-        
+        01 ws-valor-venda     pic  9(6)V99.
+        01 ws-valor-compra    pic  9(6)V99.
+        01 ws-percentual      pic  9(3)V9(12).
         procedure division.
         main.
+            display "=========================================".
             display "Calcular porcentagem entre dois valores ".
+            display "=========================================".
+            display " ".
+            display "Digite valor de compra ".
+            accept ws-valor-compra.
+            
+            display "Digite valor de venda".
+            accept ws-valor-venda.
+
+            compute  ws-percentual = ((ws-valor-venda- ws-valor-compra)  
+            / ws-valor-compra) *100 .
+            display "Resultado " ws-percentual.
         
         end program CALC1.
