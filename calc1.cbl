46561124dd39a0996267aad0cb9824e25359b1eb
sonival.santos <sonival.santos@os2h-alp-mac177.local>
2022-08-06 21:39:35 -0300

    start 1

---
+++ calc1.cbl
@@ -0,0 +1,15 @@
+        identification division.
+        program-id. CALC1.
+        *---  ESTE PROGRAMA CALCULAR DIFERENCA PERCENTUAL ENTRE VALOR 
+        
+        environment division.
+        configuration section.
+        
+        data division.
+        working-storage section.
+        
+        procedure division.
+        main.
+            display "Calcular porcentagem entre dois valores ".
+        
+        end program CALC1.
